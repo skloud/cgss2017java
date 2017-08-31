b2065f890277ad37082fe6caf7447f0fefe168ec
skloud <mnorbyn@gmail.com>
2017-08-30 19:51:07 -0500

    Add files via upload

--- MultiplicationTables.java
+++ MultiplicationTables.java
@@ -9,28 +9,39 @@ public class MultiplicationTables
         System.out.println("Welcome to Multiplication Tables");
         Scanner keyboard = new Scanner(System.in);
         System.out.println();
+        
         System.out.println("What size would you like to print to? ");
         int size = keyboard.nextInt();
-
-        for(int a = 0; a <= size; ++a)
-        {
-            System.out.printf("%2d", a);
-        }
-        System.out.println();
-        for (int b = 0; b <= size; ++b)
-        {        
-            System.out.printf("%2d", size);
-        }
-        System.out.println();
-
-        {
+        
+        keyboard.nextLine();
+        
+        System.out.print("  ");
             for(int a = 0; a <= size; ++a)
             {
-                System.out.printf("%2d", a * size);
+                System.out.printf("%7d", a);
             }
             System.out.println();
-
-        }
+            for (int b = 0; b <= size; ++b)
+            {        
+                
+                if(b<10)
+                {
+                    System.out.print(" " +b);
+                } else
+                
+                {
+                    System.out.print(b);
+                }
+                for(int a = 0; a <= size; ++a)
+                {
+                    System.out.printf("%7d", a * b);
+                }
+                System.out.println();
+            }
+            
+           
+            
+        
 
     }   
 }
